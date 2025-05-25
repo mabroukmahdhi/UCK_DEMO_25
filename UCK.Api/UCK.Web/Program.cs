8c7283dc2a83e0389b496f638770591c2d9d8fbb
Mabrouk Mahdhi <coding@mahdhi.com>
2025-05-26 01:35:50 +0200

    INFRA: Initialize Web Project

---
+++ UCK.Api/UCK.Web/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using UCK.Web.Views;
+
+namespace UCK.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+               .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
