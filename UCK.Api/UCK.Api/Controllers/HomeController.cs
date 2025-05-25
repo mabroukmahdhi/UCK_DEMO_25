f2b5664a61cd28cd81bb626c8c979e28c260ab87
Mabrouk Mahdhi <coding@mahdhi.com>
2025-05-26 01:25:53 +0200

    CODE RUB: Setup Project

---
+++ UCK.Api/UCK.Api/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace UCK.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        public string Get() =>
+            "Welcome to UCK API! This is the home controller.";
+    }
+}
--- UCK.Api/UCK.Api/Program.cs
+++ UCK.Api/UCK.Api/Program.cs
@@ -3,6 +3,8 @@ using AutoInject;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using UCK.Api.Brokers.Storages;
 
 namespace UCK.Api
 {
@@ -17,11 +19,30 @@ namespace UCK.Api
             builder.Services.AddOpenApi();
             builder.Services.UseAutoInjection();
 
+
+            builder.Services.AddDbContext<StorageBroker>();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "UCK API",
+                    Version = "v1",
+                    Description = "API documentation for UCK services"
+                });
+            });
+
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "UCK API v1");
+                    c.RoutePrefix = string.Empty; // Swagger at root
+                });
             }
 
             app.UseHttpsRedirection();
